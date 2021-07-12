cc0b8a4f52e6f2a249f25acb033bd4ccdb77c1e2
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-12 19:20:55 +0200

    palindroma

--- js/javascriptPalidroma.js
+++ js/javascriptPalidroma.js
@@ -1,3 +1,21 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+var parola = prompt('Inserisci una parola', 'radar');
+
+
+var parolaDivisa = parola.split('');
+console.log(parolaDivisa);
+
+var parolaDivisa = parolaDivisa.reverse();
+console.log(parolaDivisa);
+
+var parolaInvertita = parolaDivisa.join('');
+console.log(parolaInvertita);
+
+if(parola == parolaInvertita){
+  console.log('la parola è palindroma');
+} else {
+  console.log('la parola non è palindroma');
+}
