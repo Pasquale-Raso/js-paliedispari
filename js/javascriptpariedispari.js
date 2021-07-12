35d385ed91651584a277262e98aef7384b750f76
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-12 15:04:52 +0200

    BASE

---
+++ js/javascriptpariedispari.js
@@ -0,0 +1,15 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// 1 chiedi all'utente di scegliere tra pari e dispari
+// 2 chiedi all'utente di scegliere un numero da 1 a 5
+// 3 creare una funzione che generi un numero random da 1 a 5 (FUNZIONE)
+// 4 sommiamo i due numeri (quello dell'utente e quello del pc)
+// 5 creare una funzione che stabilisca se la somma è pari o dispari (FUNZIONE)
+// 6 dichiara chi ha vinto
+
+
